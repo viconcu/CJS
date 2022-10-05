dad1b78d4b63a1107ff54233300e16b0411a4ed5
viconcu <114194792+viconcu@users.noreply.github.com>
2022-10-05 16:21:11 -0300

    Add files via upload

---
+++ shopping/index.js
@@ -0,0 +1,16 @@
+import { mostrarProductos } from "./App.js";
+import { pintarCarrito } from "./src/accionesCarrito.js";
+import { actualizarTotalesCarrito } from "./src/actualizarCarrito.js";
+import { productos } from './src/stock.js';
+import { obtenerCarritoStorage } from "./src/storage.js";
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    mostrarProductos(productos);
+
+    if (localStorage.getItem('carrito')) {
+        const carrito = obtenerCarritoStorage();
+        pintarCarrito(carrito);
+        actualizarTotalesCarrito(carrito);
+    };
+});
